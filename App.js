52420daf8db6dd57aacad74c9978ee90eeafd3ab
jacobsteves <jgsteves@uwaterloo.ca>
2017-12-28 22:22:21 -0500

    added basic interface and functionality

--- App.js
+++ App.js
@@ -1,16 +1,82 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput
+} from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+        super();
+        this.state = {
+            myNumber: "0",
+            gradeArray: [{"grade":0,"value":0}]
+        };
+  }
+
+  onChanged(position, gradeInput, text){
+        var newText = '';
+        var numbers = '0123456789';
+        var newNumber = "";
+        var gradeArray = this.state.gradeArray;
+
+        for (var i=0; i < text.length; i++) {
+            if(numbers.indexOf(text[i]) > -1 ) {
+                newText = newText + text[i];
+            }
+            newNumber = newText;
+        }
+
+        if (gradeInput) gradeArray[position].grade = newNumber;
+        else gradeArray[position].value = newNumber;
+
+        this.setState({ gradeArray: gradeArray });
+  }
+
+  renderTextInputs() {
+    const gradeArray = this.state.gradeArray;
+    return gradeArray.map((object, i) => {
+                return (
+                    <View style={styles.row}>
+                        <TextInput
+                            placeholder="0"
+                            keyboardType="numeric"
+                            onChangeText={(text)=> this.onChanged(i, true, text)}
+                            value={this.state.gradeArray[i].grade}
+                        />
+                        <TextInput
+                            placeholder="0"
+                            keyboardType="numeric"
+                            onChangeText={(text)=> this.onChanged(i, false, text)}
+                            value={this.state.gradeArray[i].value}
+                        />
+                    </View>
+                );
+        });
+  }
+
+  addNewGrade() {
+    let { gradeArray } = this.state;
+    let newGrade = {
+        "grade": 0,
+        "value": 0
+    };
+    gradeArray.push(newGrade);
+
+    this.setState({ gradeArray: gradeArray });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Test.</Text>
+        {this.renderTextInputs()}
+        <Text onPress={() => this.addNewGrade()}>Add a grade.</Text>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +85,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    flexDirection:'row',
   },
 });
